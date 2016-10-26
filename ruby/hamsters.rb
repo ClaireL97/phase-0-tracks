b49c36baa98acd593bc6575d0869c7c8334147aa
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-10-26 11:32:23 -0700

    ask if hamster is good candidate and save input

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,9 +1,15 @@
 puts "What is the hamsters name?"
 hamster_name = gets.chomp
 #p hamster_name
+
 puts "On a scale of 1 to 10 with 10 being loud, how noisy is the hamster?"
 volume_level = gets.chomp
 #p volume_level
+
 puts "What color is this hamster?"
 fur_color = gets.chomp
 p fur_color
+
+puts "Is this hamster a good candidate for adoption? Yes or No?"
+candidate = gets.chomp
+p candidate
