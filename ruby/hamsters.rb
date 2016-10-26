bb45c309ad445a4a4036fac0228944c17125ad11
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-10-26 11:28:51 -0700

    ask for noise level and save input

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,3 +1,6 @@
 puts "What is the hamsters name?"
 hamster_name = gets.chomp
-p hamster_name
+#p hamster_name
+puts "On a scale of 1 to 10 with 10 being loud, how noisy is the hamster?"
+volume_level = gets.chomp
+p volume_level
