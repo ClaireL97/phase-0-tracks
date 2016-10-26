1a15a18c4ad5aeae45597ec8c5acca886b0fa6ba
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-10-26 11:29:54 -0700

    ask for fur color and save input

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,4 +3,7 @@ hamster_name = gets.chomp
 #p hamster_name
 puts "On a scale of 1 to 10 with 10 being loud, how noisy is the hamster?"
 volume_level = gets.chomp
-p volume_level
+#p volume_level
+puts "What color is this hamster?"
+fur_color = gets.chomp
+p fur_color
