5689ed9a9c43ef477c0f5e6a61022bf84a50ccd9
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-10-26 12:18:48 -0700

    add a conditional to if hamster is ready

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,17 +3,30 @@ hamster_name = gets.chomp
 #p hamster_name
 
 puts "On a scale of 1 to 10 with 10 being loud, how noisy is the hamster?"
-volume_level = gets.chomp
+volume_level = gets.to_i
 #p volume_level
 
 puts "What color is this hamster?"
 fur_color = gets.chomp
-p fur_color
+#p fur_color
+
+
+#puts "How old is this hamster?"
+#age = gets.to_i
+#if age = 
+	#puts "unknown"
+#else 
+	#age = gets.to_i
+#end
+#p age
 
 puts "Is this hamster a good candidate for adoption? Yes or No?"
 candidate = gets.chomp
-#p candidate
+if candidate == "Yes"
+	ready = "This hamster is ready for adoption"
+	
+elsif candidate == "No"
+	ready = "Needs more time in sanctuary"
+end
 
-puts "How old is this hamster?"
-age = gets.chomp
-#p age
+p "This hamsters name is #{hamster_name}. On a scale of one to 10 they are a #{volume_level}. #{hamster_name} is #{fur_color} and  years old. #{ready}."
