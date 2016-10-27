38416b848fcaf2ce9c09e0b60ffa30c870988680
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-10-27 15:54:59 -0700

    add first version of age checker

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,6 +7,18 @@ age = gets.chomp
 puts "What year were you born in?"
 year = gets.chomp
 
+current_year = 2016
+#p current_year
+
+if current_year-year != age
+	correct_age = false
+else current_year = age
+	correct_age = true
+end
+
+puts correct_age
+
+
 puts "Our company cafeteria serves garlic bread. Should we order some for you? Yes or No?"
 bread_pref = gets.chomp
 
