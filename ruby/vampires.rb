8ec2b300fb937a6bff9d3b67f8379d7acbfd0bce
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-10-28 08:06:56 -0700

    wrote first part of logic checker

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,7 +2,7 @@ puts "What is your name?"
 name = gets.chomp
 #p name
 puts "How old are you?"
-age = gets.chomp
+age = gets.to_i
 
 puts "What year were you born in?"
 year = gets.to_i
@@ -10,16 +10,14 @@ year = gets.to_i
 current_year = 2016
 #p current_year
 
-#update this later in order to subtract the current year from the given year to get a new variable to compare to age
-#year_diff = current_year-year
-#p year_diff
+year_diff = current_year-year
+#p year_diff 
 
-#if year_diff != age
-	#correct_age = false
-#elsif year_diff == age
- 	# year_diff == age
-	#correct_age = true
-#end
+if year_diff == age
+	correct_age = true
+elsif year_diff != age
+	correct_age = false
+end
 
 #puts correct_age
 
@@ -32,7 +30,7 @@ elsif bread_pref == "No"
 	garlic = false
 end
 
-p garlic
+#p garlic
 
 puts "Would you like to enroll in the company's health insurance? Yes or No?"
 h_insurance = gets.chomp
@@ -42,21 +40,30 @@ if h_insurance == "Yes"
 elsif h_insurance == "No"
 	insurance = false
 end
+
+#p insurance
+
+#Ask jeremy about this one
+if correct_age && (garlic||insurance)
+	puts "probably not a vampire"
+
+elsif !correct_age && (!garlic||!insurance)
+	puts "Might be a vampire"
+elsif !correct_age && (!garlic && !insurance)
+	puts "Almost certainly a vampire"
+
+elsif name == "Drake Cula"
+	puts "Definitely a vampire"
+
+elsif name == "Tu Fang"
+	puts "Definitely a vampire"
 	
 
+else
+	puts "Results inconclusive"	
+end
 
-#if correct_age && (garlic||insurance)
-#	puts "probably not a vampire"
-
-#elsif correct_age && (garlic||insurance)
-#	puts "Might be a vampire"
-	
-#else 
-#	puts ""
-#end
-	
-
-
+stop_word = done
 
 
 
