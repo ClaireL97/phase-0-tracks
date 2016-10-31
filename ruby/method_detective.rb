fe0fe669eb7413c9a68a018c736a0c8c8fc8d3b7
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-10-31 12:40:18 -0700

    make method count letter a in a string

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -35,5 +35,5 @@ puts "Stop! You’re under arrest!".upcase
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
