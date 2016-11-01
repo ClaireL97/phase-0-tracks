0662c228a359f377c1958593ed1183bc598559ea
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-10-31 23:02:46 -0700

    made letter dissapear

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,37 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-puts "iNvEsTiGaTiOn".swapcase
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-puts "zom".insert(2, 'o')
+p "zom".insert(2, 'o')
 # => “zoom”
 
-puts "enhance".center(20)
+p "enhance".center(20)
 # => "    enhance    "
 
-puts "Stop! You’re under arrest!".upcase
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-puts "the usual".insert(-1, ' suspects')
+p "the usual".insert(-1, ' suspects')
 #=> "the usual suspects"
 
-puts " suspects".insert(0, 'the usual')
+p " suspects".insert(0, 'the usual')
 # => "the usual suspects"
 
-puts "The case of the disappearing last letter".chop
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-#puts "The mystery of the missing first letter"
+p "The mystery of the missing first letter".slice!(1..-1)
 # => "he mystery of the missing first letter"
 
 p "Elementary,    my   dear        Watson!".squeeze(' ')
 # => "Elementary, my dear Watson!"
 
-puts "z".getbyte(0)
+p "z".getbyte(0)
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 # 122 is the alt code number for lowercase z
 
-puts "How many times does the letter 'a' appear in this string?".count "a"
+p "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
