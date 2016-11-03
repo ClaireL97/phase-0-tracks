91e8fe7deab0d9bdfb0e08f0ac10dde2b566b8af
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-03 09:37:34 -0700

    make nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,28 @@
+Inuyasha_characters = {
+	Inuyasha:{
+		Powers:["Wind Scar","Iron Reaver Soul Stealer","Backlash Wave", "Barrier Breaking","Adament Shards"]
+		Weapons:["Tensaiga"]
+	}
+
+	Sesshomaru:{
+		Powers:["Miasma","Demon Dog Form","Poision Claws"]
+		Weapons:["Tenseiga", "Tokijin"]
+	}
+
+	Sango:{
+		Powers:["Hiaikotsu"]
+		Weapons: ["Boomerang","Short Sword"]
+	}
+
+	Miroku:{
+		Powers:["Wind Tunnel"]
+		Weapoons: ["Staff", "His Hand"]
+	}
+
+	Shippo:{
+		Powers:["Transformation", "Fox Magic"]
+		Weapons: []
+
+	}
+}
+p Inuyasha_characters[:Inuyasha][:Powers][2]
