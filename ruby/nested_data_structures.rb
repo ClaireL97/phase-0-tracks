2c7a552251152691aa3fa0422bb684ffd9235d5b
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-04 14:13:04 -0700

    created nested data structure to manipulate

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,35 @@
+Classroom = {
+    staff_desk: {
+        desk_owner: 'Teacher',
+        supplies: [
+            "Paper",
+            "Sticky-Notes",
+            "Pens", 
+            "Attendence Sheet",
+            "Computer"
+        ]
+    },
+
+    desk_1: {
+        desk_owner: 'Female Student',
+       	supplies: [
+            "Jell-Pens",
+            "Compact Mirror",
+            "Colored Pencils",
+            "Markers",
+            "Notebook",
+            "Normal Pens and Pencils"
+        ]
+    },
+    desk_2: {
+        desk_owner: 'Male Student',
+        supplies: [
+        	"Pens and Pencils",
+        	"Markers",
+        	"Notebooks",
+        	"Snacks"
+        ]
+    }
+}
+
+#Mirror's are not allowed in desks so taking mirror away from female desks.
