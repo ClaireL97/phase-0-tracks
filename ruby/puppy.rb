bd9d1b4d91928d922987a34fd0e758d9f841dc50
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-07 14:13:55 -0800

    add characteristics and behavious

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+#Species-------
+#Adorable Puppy
+
+#Characteristics -------
+#Ear perk:varies
+	#(It can be one up and one down, both up or both floppy)
+#nose color:varies
+#collar color:varies
+#friendly: definitely
+#tounge: wet
+#lick tendency : varies
+
+
+#behaviors-------
+#fetch
+#run
+#jump
+#roll over
+#beg for treats
+#get belly rubs and pets
