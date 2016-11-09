3fbe77cba2fb05c4cc0744c09ec49a17c579253c
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-09 12:01:47 -0800

    create a mixin module and write driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin 
 module Shout
     def self.yell_angrily(words)
     	words + "!!!" + " :("
@@ -8,7 +9,34 @@ module Shout
   	end
 end
 
-
-
 p Shout.yell_happily("I FINALLY BEAT THIS GAME")
 p Shout.yell_angrily("WHY YOU DO THIS")
+=end
+
+module Shout
+	def yell_angrily(words)
+		puts "#{words}!!! D:< "
+	end
+
+	def yell_happily(words)
+		puts "#{words}!!!!! :D "
+	end
+end
+
+#two classes that have shout method
+class Gamer
+	include Shout
+end
+
+class Teacher
+	include Shout
+end
+
+#driver code
+gamer = Gamer.new
+gamer.yell_angrily("How did we lose when we were winning earlier")
+gamer.yell_happily("Awesome We made a comeback")
+
+teacher = Teacher.new
+teacher.yell_angrily("Finish your homework")
+teacher.yell_happily("Have a good summer vacation")
