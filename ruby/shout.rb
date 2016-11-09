ee793f6f0d1c372580b4e78f49c6965786c09390
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-09 11:44:39 -0800

    create driver code to call methods in the module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,14 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+    def self.yell_angrily(words)
+    	words + "!!!" + " :("
+  	end
+
+  	def self.yell_happily(words)
+  		words + "!!!!!" + " :D "
+  	end
 end
+
+
+
+p Shout.yell_happily("I FINALLY BEAT THIS GAME")
+p Shout.yell_angrily("WHY YOU DO THIS")
