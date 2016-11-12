5376f2c6612d658719985d1a8f740fa9ab1c7b38
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-12 15:44:33 -0800

    create method to turn the given word into a blank array

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,8 +1,21 @@
 class Word_Game
-	def initialize
+	def initialize(word)
+		puts "Initializing word game instance..."
+		@Word = word.upcase
+		@word_characters = word.upcase.split('')
+		@Guess_list = []
+		@Answer = []
 	end
 
+	def answer_box
+		length = 0
+		until length == @Word.length
+			@Answer.push ('_')
+			length += 1
+		end
+		p @Answer
+	end
 
-
-
+	 def guess_check(guess)
+	 end
 end
