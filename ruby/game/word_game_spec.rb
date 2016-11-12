986ecfcbfa7d520ef595d1455385e24d9e834220
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-12 15:44:52 -0800

    tested the blank answer method

--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -1,4 +1,10 @@
 require_relative 'word_game'
 
-describe Calculator do
-  let(:word_game) {Word_Game.new}
+describe Word_Game do
+  let(:word_game) {Word_Game.new("Shelter")}
+
+ it "takes word length and turns it into an array of underscores" do
+    expect(word_game.answer_box).to eq ["_", "_", "_", "_", "_", "_", "_"]
+  end
+
+end
