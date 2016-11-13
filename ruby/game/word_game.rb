c0daf7e3870cf30ab0d99c9ac94efbd14cfe5893
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-12 21:25:51 -0800

    add a check to see if word itself is guesed

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -3,9 +3,10 @@ class Word_Game
 		puts "Initializing word game instance..."
 		@Word = word.upcase
 		@word_characters = word.upcase.split('')
+		@word_index = {}
 		@Guess_list = []
 		@Answer = []
-		@guess_limit = 20
+		@guess_limit = @Word.length
 	end
 
 	def get_Guess_list
@@ -20,16 +21,21 @@ class Word_Game
 			length += 1
 		end
 		p @Answer
-	end
+	end	
+
 
 	def guess_checker(guess)
 		user_guess = guess.upcase
 		if @Guess_list.include?user_guess
 			puts "You have already guessed that"
 
+		elsif user_guess.upcase == @Word
+			puts "Nice job, the word was #{@Word}! You Win!"
+
 		elsif @word_characters.include?user_guess
 			@Guess_list.push(user_guess)
 			character_index = @word_characters.index(user_guess)
+			p character_index
 			@Answer[character_index] = user_guess
 			p @Answer
 			p @Guess_list
@@ -40,22 +46,19 @@ class Word_Game
 			@Guess_list.push(user_guess)
 			p @Answer
 			p @Guess_list
-			@guess_limit -= 1
-			
+			@guess_limit -= 1		
 		end
-
-	#Things to do
-		#Make program find all the index's for example in letter 2 t's and right now program only takes first one
-		#make it print answers for when answer is reached
-
 	end
 
 
 
 end
-
+#Things to do
+		#Make program find all the index's for example in letter 2 t's and right now program only takes first one
+		#make it print answers for when answer is reached
 game = Word_Game.new("Shelter")
 game.answer_box
-game.guess_checker("y")
+game.guess_checker("SHELTER")
+game.guess_checker("s")
 game.guess_checker("t")
 game.guess_checker("s")
