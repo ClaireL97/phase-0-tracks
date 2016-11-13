b6ff0d245603c00791475a35a4fa6879dbf969e8
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-13 15:41:53 -0800

    add a test for a wrong guess

--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -4,13 +4,23 @@ describe Word_Game do
   let(:word_game) {Word_Game.new("Shelter")}
 
  it "takes word length and turns it into an array of underscores" do
-    expect(word_game.answer_box).to eq ["_", "_", "_", "_", "_", "_", "_"]
+    expect(word_game.Answer).to eq ["_", "_", "_", "_", "_", "_", "_"]
   end
 
   it "guesses the letter E " do
-  	word_game.answer_box
   	word_game.guess_checker("E")
   	expect(word_game.Answer).to eq ["_","_","E","_","_","E","_"]
   end
+  
+ it "guesses the full word" do
+ 	word_game.guess_checker("shelter")
+ 	expect(word_game.Answer).to eq (word_game.Word)
+ end
+
+ it "Guesses a wrong answer" do 
+ 	expect(word_game.guess_checker("M")). to eq ("You have #{word_game.guess_limit} guesses left")
+ end
+
+
 
 end
