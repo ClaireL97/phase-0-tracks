4724b1941a9c04558878184dc2169b9a1e84eda3
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-14 17:56:17 -0800

    add explanation about private method

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -21,19 +21,23 @@ class VirusPredictor
 
   #Virus_effects method calls the other two methods and runs them with the specified attributes.
   def virus_effects
-    # predicted_deaths(@population_density, @population, @state)
-    # speed_of_spread(@population_density, @state)
     predicted_deaths
     speed_of_spread
+  #commented out the old code to make it more DRY
+    # predicted_deaths(@population_density, @population, @state)
+    # speed_of_spread(@population_density, @state)
   end
 
-  private
+  private 
+  #This private method means that code below it can only be accessed inside the class that defines them. This means that the only way
+  #these files can run is when called by a method in the same class but outside the private method (example. Virus_effects). If the private tag was
+  #Moved above virus_effects in this situation, it would mean that we couldn't call it outside the class (example. current_state.virus_method) would not work.
 
   #Predicted_deaths method takes the population density of the specified state and depending on the number runs a calculation based
   # on population that is rounded down to the nearest interger and saves it as the variable number_of_deaths. This variable is later
   # printed out using state name and number of deaths.
 
-  def predicted_deaths #(population_density, population, state)
+  def predicted_deaths #(population_density, population, state) removed un-needed arguments
     # predicted deaths is solely based on population density
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
@@ -54,7 +58,9 @@ class VirusPredictor
 #The speed_of_spread metod takes the given state and uses the population density variable to determine what the speed of spread difference would be.
 #The speed is a variable in months and depending on the density, the speed is increased by a set value. Then the method prints out spread speed.
 
-  def speed_of_spread #(population_density, state) #in months
+  def speed_of_spread #(population_density, state)in months 
+    #I removed un-needed arguments to make it more DRY
+
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
