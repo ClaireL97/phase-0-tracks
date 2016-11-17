dafbc29286031ee6b99290f1ec7dca9b7e194b71
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-17 15:04:43 -0800

    create a function to reverse a string

--- js/explore.js
+++ js/explore.js
@@ -2,3 +2,12 @@
 // turn the argument into an array
 // reverse the array
 // join the array together in reverse order
+
+
+function reverse(string_of_choice){
+	splitstring = string_of_choice.split('')
+	reversed_split = splitstring.reverse()
+	return reversed_split.join('');
+}
+
+reverse("Hello World")
