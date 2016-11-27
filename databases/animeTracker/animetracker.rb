193d8bb3d05dc8ded85527a3eea0177dd9418b24
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-27 12:31:49 -0800

    create add anime method

--- databases/animeTracker/animetracker.rb
+++ databases/animeTracker/animetracker.rb
@@ -1,13 +1,20 @@
 require 'sqlite3'
  
 db = SQLite3::Database.new("animelist.db")
+db.results_as_hash = true
 
-create_ table_cmd = <<-SQL
+create_ table_command = <<-SQL
 	CREATE TABLE IF NOT EXISTS animelist(
 		id INTEGER PRIMARY KEY,
 		title VARCHAR(255),
-		episode_number INT,
+		total_episodes INT,
 		current_episode INT,
-		complete BOOLEAN
+		completed BOOLEAN
 	)
 SQL
+
+db.execute(create_table_command)
+
+def add_anime(db,title,total_episodes,current_episode,completed)
+	db.execute("INSERT INTO animelist(title,total_episodes,current_episode,completed VALUES(?,?,?,?)"[title,total_episodes,current_episode,completed])
+end
