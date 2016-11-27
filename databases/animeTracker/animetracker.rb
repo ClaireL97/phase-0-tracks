e1cf7034bef38622d4f74f24cb9bc035aa82f9d1
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-27 14:26:06 -0800

    add data to table in database and make it print result

--- databases/animeTracker/animetracker.rb
+++ databases/animeTracker/animetracker.rb
@@ -18,3 +18,22 @@ db.execute(create_table_command)
 def add_anime(db,title,total_episodes,current_episode,completed)
 	db.execute("INSERT INTO animelist(title,total_episodes,current_episode,completed VALUES(?,?,?,?)"[title,total_episodes,current_episode,completed])
 end
+
+
+#creating data for the table
+add_anime(db,"Mahou Shoujo Ikusei Keikaku",12,9,false)
+add_anime(db,"Log Horizon",25,25,true)
+add_anime(db,"Nanatsu no Taizai",24,24,true)
+add_anime(db,"Shokugeki no Soma",24,24,true)
+
+
+#Data printing
+anime = db.execute("SELECT * FROM animelist")
+anime.each do |anime|
+	puts "#{anime['title']} is #{anime['total_episodes']} episodes long and I am on episode #{current_episode}.}"
+	if anime['completed'] == true
+		puts "I finished this anime"
+	else 
+		puts "I still need to finish this one"
+	end
+end 
