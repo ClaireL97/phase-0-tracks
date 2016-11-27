434f07647727b439fe10f6753fa99703baaa341a
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-27 11:39:41 -0800

    wrote a create table command to make new table in database

--- databases/animeTracker/animetracker.rb
+++ databases/animeTracker/animetracker.rb
@@ -1,1 +1,13 @@
 require 'sqlite3'
+ 
+db = SQLite3::Database.new("animelist.db")
+
+create_ table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS animelist(
+		id INTEGER PRIMARY KEY,
+		title VARCHAR(255),
+		episode_number INT,
+		current_episode INT,
+		complete BOOLEAN
+	)
+SQL
