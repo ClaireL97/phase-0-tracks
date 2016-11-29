7996f920f008a90f2526cc331f9a60b1acbe1e67
Claire C. Lindblom <claire.lindblom@gmail.com>
2016-11-29 11:53:52 -0800

    make clicker respond more than once and loop though colors

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -8,19 +8,34 @@ function rainbowFont(color_in) {
     var raccoon_text = document.getElementById("raccoon_text");
     raccoon_text.style.color = color_out;
     console.log("working "+ color_out);
-    color_index ++;
+    
   }
 
+var color_index = 0 ;
+
+ function rainbowWrap(){
+ 	var color_array = ["red","blue","orange","magenta","green","yellow","purple"];
+	rainbowFont(color_array[color_index]);
+	color_index ++;
+	if (color_index==7){
+		color_index = 0;
+	}
+ }
 // function colorIncrement{
 //   color_index++;
 // }
 
 
-var color_array = ["red","blue","orange","magenta","green","yellow","purple"];
-var color_index = 0    
+// var color_array = ["red","blue","orange","magenta","green","yellow","purple"];
+// var color_index = 0    
 var photo = document.getElementById("raccoon_suit");
 
-photo.addEventListener("click",rainbowFont(color_array[color_index]));
+
+photo.addEventListener("click",rainbowWrap,false);
+
+
+
+
 
 
 // var raccoontext = document.getElementById("raccoon_console");
